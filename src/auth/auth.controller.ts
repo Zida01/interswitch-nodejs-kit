f85b157425bb9204b6c6335bda5fee53d144a3f7
Zida01 <olaty21bisi@gmail.com>
2025-06-17 06:09:40 +0100

    added auth cntrl and service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from 'express'
+import { ResponseUtils } from '../_lib/response.utils'
+import { LoggerUtils } from '../_lib/logger.utils'
+import { authService } from './auth.service'
+import { LoginDto, RegisterDto } from './dto/auth.dto'
+
+
+
+export class authController {
+  /**
+   * @description : Initiate payment
+   * @param req
+   * @param res
+   */
+  private Authservice: authService;
+
+  constructor() {
+    this.Authservice = new authService();
+  }
+    Register = (req: Request<{}, {}, RegisterDto>, res: Response) => {
+        const{email,password, phoneNumber}= req.body
+    try {
+      const result = this.Authservice.RegisterService(req.body);
+      res.status(200).json(result);
+    } catch (err) {
+        LoggerUtils.error(err);
+        res
+          .status(400)
+          .json(
+            ResponseUtils.handleResponse(
+              false,
+              "Unable to process request",
+              null
+            )
+          );
+    }
+  };
+}
